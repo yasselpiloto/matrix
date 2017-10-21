e35cf51b8f2135a09bf0762a79709969aa18a5c6
Jazz <yassel_piloto@ultimatesoftware.com>
2017-10-21 14:35:17 -0400

    add capture

---
+++ capture.py
@@ -0,0 +1,18 @@
+import time
+from picamera import PiCamera
+
+def capture_images(save_folder):
+    """Stream images off the camera and save them."""
+    camera = PiCamera()
+    camera.resolution = (320, 240)
+    camera.vflip = True
+    camera.framerate = 10
+
+    # Warmup...
+    time.sleep(2)
+
+    # And capture continuously forever.
+    for _ in camera.capture_continuous(save_folder + '{timestamp}.jpg', 'jpeg', use_video_port=True):
+        pass
+
+capture_images('toma2')
