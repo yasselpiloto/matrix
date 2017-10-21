e479c856aa5b7a121a0cc4f42ad0b976b34e66f0
Jazz <yassel_piloto@ultimatesoftware.com>
2017-10-21 14:55:39 -0400

    capture to queue

--- capture.py
+++ capture.py
@@ -13,7 +13,14 @@ def capture_images(save_folder):
     time.sleep(2)
 
     # And capture continuously forever.
-    for _ in camera.capture_continuous(save_folder + '{timestamp}.jpg', 'jpeg', use_video_port=True):
-        pass
+    #for _ in camera.capture_continuous(save_folder + '{timestamp}.jpg', 'jpeg', use_video_port=True):
+    #    pass
+
+
+    for _, image in enumerate(camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
+        # Get the numpy version of the image.
+        decoded_image = image.array
+        serialized = pickle.dumps(decoded_image, protocol=0)
+        message_producer.publish_message(str(serialized))
 
 capture_images(os.environ['CAPTURE_PATH'])
