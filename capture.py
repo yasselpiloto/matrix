ff6c483e4ca779a936fa75b3cd5fa4239b742c3c
Jazz <yassel_piloto@ultimatesoftware.com>
2017-10-21 19:32:04 -0400

    save regular jpeg

--- capture.py
+++ capture.py
@@ -1,12 +1,12 @@
-import time
-import logging
-import pickle
+import logging.config, logging
 import os
-from messaging.AsyncMessageProducer import AsyncMessageProducer
-from messaging.AsyncMessageConsumer import AsyncMessageConsumer
-from Orchestrator import Orchestrator
+import time
+from datetime import datetime
 import picamera
 import picamera.array
+
+from messaging.AsyncMessageConsumer import AsyncMessageConsumer
+from messaging.AsyncMessageProducer import AsyncMessageProducer
 
 LOGGER = logging.getLogger(__name__)
 
@@ -39,24 +39,19 @@ def capture_images(save_folder):
     #    time.sleep(2)
 
     logging.config.fileConfig('config/logging.conf', disable_existing_loggers=False)
-    message_consumer, message_producer = build_queue_integration()
-    orchestrator = Orchestrator(message_consumer, message_producer)
-    orchestrator.start()
+    # message_consumer, message_producer = build_queue_integration()
+    # orchestrator = Orchestrator(message_consumer, message_producer)
+    # orchestrator.start()
     with picamera.PiCamera() as camera:
-        with picamera.array.PiRGBArray(camera) as output:
-            camera.resolution = (320, 240)
-            camera.vflip = True
+        camera.resolution = (320, 240)
+        camera.vflip = True
+        while True:
+            filename = save_folder + datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f") + '.jpeg'
+            print(filename)
+            camera.capture(filename, 'jpeg')
+            time.sleep(0.2)
 
-            camera.capture(output, 'bgr')
-            decoded_image = output.array
-            pickle.dump(decoded_image, open("image-pi", "wb"), protocol=0)
-            serialized = pickle.dumps(decoded_image, protocol=0)
-
-            time.sleep(0.1)
-
-            message_producer.publish_message(serialized)
-
-            output.truncate(0)
+            # message_producer.publish_message(serialized)
 
 
 capture_images(os.environ['CAPTURE_PATH'])
